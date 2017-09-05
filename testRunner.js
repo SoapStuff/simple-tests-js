b3ef3d8e196faf81296efc783e05320383f43aab
Arjan Seijs <A.M.Seijs@student.tudelft.nl>
2017-09-05 17:15:14 +0200

    Refractored testRunner for better structure.

--- testRunner.js
+++ testRunner.js
@@ -1,84 +1,100 @@
-const path = require("path").join(__dirname, "");
-const results = {
-    passed: 0,
-    failed: 0,
-    failedTests: []
-};
+var passed = 0;
+var failed = 0;
+var failedTests = [];
+
 module.exports.getResults = function() {
     return results;
 };
 /**
- * Execute the tests
- * @param tests
- * @param file
+ * Execute the tests and logs the results.
+ * @param tests - An object with al the tests.
+ * @param file - The file the tests is run from.
  */
 module.exports.run = function (tests,file) {
-    var passed = 0;
-    var failed = 0;
-    var failedTests = [];
+    new TestRunner(tests,file).run();
+};
 
-    for (var k in tests) {
-        if (tests.hasOwnProperty(k)) {
-            // Run the test
-            if (typeof tests[k] === "function") {
-                try {
-                    tests[k]();
-                    succeed()
-                } catch (error) {
-                    fail(error,error.toString());
-                }
-            }
-            // Run parametrized test
-            if (typeof tests[k] === "object") {
-                if (tests[k].parameters && tests[k].test) {
-                    for(var i = 0; i < tests[k].parameters.length; i++) {
-                        try {
-                            tests[k].test(tests[k].parameters[i]);
-                            succeed()
-                        }
-                        catch (error) {
-                            fail(error,"Test Failed with parameters:"+JSON.stringify(tests[k].parameters[i]) + " " + error.toString());
-                        }
-                    }
-                } else {
-                    throw new Error("A parametrized test should be in the format: {test: (function), parameters: (Array(Array(parameters))");
-                }
+module.exports.getPassed = function() {
+    return passed;
+};
+module.exports.getFailed = function() {
+    return failed;
+};
+module.exports.getFailedTests = function() {
+    return failedTests
+};
+
+function TestRunner(tests,file) {
+    this.tests = tests;
+    this.file = file;
+    this.passed = 0;
+    this.failed = 0;
+    this.failedTests = [];
+    //TODO validate tests & file.
+}
+
+TestRunner.prototype.constructor = TestRunner;
+TestRunner.prototype.run = function() {
+    for(var key in this.tests) {
+        if (this.tests.hasOwnProperty(key)) {
+            switch (typeof this.tests[key]) {
+                case "function": this.executeTest(this.tests[key],key.toString()); break;
+                case "object": this.executeParamTest(this.tests[key],key.toString()); break;
             }
         }
     }
-    printResults(passed,failed,failedTests,file);
-
-    //Inner function
-    function fail(error,message) {
-        var testResult = {
-            file: file,
-            test: k,
-            message: message,
-            stack: error.stack
-        };
-        failedTests.push(testResult);
-        results.failedTests.push(testResult);
-        failed++;
-        results.failed++;
-    }
-
-    function succeed(){
-        passed++;
-        results.passed++;
+    this.printResults();
+};
+TestRunner.prototype.executeTest = function(test,testName) {
+    try {
+        test();
+        this.succeedTest()
+    } catch (error) {
+        this.failTest(error,error.toString(),testName);
     }
 };
-
-function printResults(passed,failed,failedTests,file) {
-    var string = "======== " + file + " ========";
+TestRunner.prototype.executeParamTest = function(test,testName) {
+    if (test.parameters && test.test) {
+        for(var i = 0; i < test.parameters.length; i++) {
+            try {
+                test.test(test.parameters[i]);
+                this.succeedTest()
+            }
+            catch (error) {
+                this.failTest(error,"Test Failed with parameters:"+JSON.stringify(test.parameters[i]) + " " + error.toString(),testName);
+            }
+        }
+    } else {
+        console.log("Failed test " + testName + "because of invalid format.");
+    }
+};
+TestRunner.prototype.failTest = function(error,message,testName) {
+    this.failed++;
+    failed++;
+    var testResult = {
+        file: this.file,
+        test: testName,
+        message: message,
+        stack: error.stack
+    };
+    this.failedTests.push(testResult);
+    failedTests.push(testResult);
+};
+TestRunner.prototype.succeedTest = function() {
+    this.passed++;
+    passed++;
+};
+TestRunner.prototype.printResults = function() {
+    var string = "======== " + this.file + " ========";
     console.log(string);
-    console.log("Passed Tests: " + passed);
-    console.log("Failed Tests: " + failed);
-    if(failed > 0) console.log("Failed Tests: " +"\n");
-    for(var i = 0; i < failedTests.length; i++) {
-        console.log(" Test: " + failedTests[i].test);
-        console.log(" Message: " + failedTests[i].message);
-        console.log(" Stack trace: " + failedTests[i].stack +"\n");
+    console.log("Passed Tests: " + this.passed);
+    console.log("Failed Tests: " + this.failed);
+    if(this.failed > 0) console.log("Failed Tests: " +"\n");
+    for(var i = 0; i < this.failedTests.length; i++) {
+        console.log(" Test: " + this.failedTests[i].test);
+        console.log(" Message: " + this.failedTests[i].message);
+        console.log(" Stack trace: " + this.failedTests[i].stack +"\n");
     }
     // I just want things to look pretty :)
     console.log(string.replace(/\D/g,"=").replace(/\d/g,"=") +"\n");
-}
+};
