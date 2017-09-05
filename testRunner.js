fdb446b1b778f8c6538e19886ac5452c7cd558c0
Arjan Seijs <A.M.Seijs@student.tudelft.nl>
2017-09-05 17:24:15 +0200

    Added documentation.

--- testRunner.js
+++ testRunner.js
@@ -13,17 +13,34 @@ module.exports.getResults = function() {
 module.exports.run = function (tests,file) {
     new TestRunner(tests,file).run();
 };
-
+/**
+ * Get amount of passed tests.
+ * @return {number} - The amount of passed Tests.
+ */
 module.exports.getPassed = function() {
     return passed;
 };
+/**
+ * Get amount of failed tests.
+ * @return {number}
+ */
 module.exports.getFailed = function() {
     return failed;
 };
+/**
+ * Get a list of all failed tests.
+ * @return {Array}
+ */
 module.exports.getFailedTests = function() {
     return failedTests
 };
 
+/**
+ * Creates a TestRunner class.
+ * @param tests - A object with all the tests.
+ * @param {string} file - The name of the file where it is run from.
+ * @constructor
+ */
 function TestRunner(tests,file) {
     this.tests = tests;
     this.file = file;
@@ -34,6 +51,9 @@ function TestRunner(tests,file) {
 }
 
 TestRunner.prototype.constructor = TestRunner;
+/**
+ * Executes the tests.
+ */
 TestRunner.prototype.run = function() {
     for(var key in this.tests) {
         if (this.tests.hasOwnProperty(key)) {
@@ -45,6 +65,11 @@ TestRunner.prototype.run = function() {
     }
     this.printResults();
 };
+/**
+ * Executes a test.
+ * @param test - The test to execute.
+ * @param testName - The name of the test.
+ */
 TestRunner.prototype.executeTest = function(test,testName) {
     try {
         test();
@@ -53,6 +78,11 @@ TestRunner.prototype.executeTest = function(test,testName) {
         this.failTest(error,error.toString(),testName);
     }
 };
+/**
+ * Executes an Parametrized test.
+ * @param test - The test to execute.
+ * @param testName - The name of the test.
+ */
 TestRunner.prototype.executeParamTest = function(test,testName) {
     if (test.parameters && test.test) {
         for(var i = 0; i < test.parameters.length; i++) {
@@ -68,6 +98,12 @@ TestRunner.prototype.executeParamTest = function(test,testName) {
         console.log("Failed test " + testName + "because of invalid format.");
     }
 };
+/**
+ * Fails a test.
+ * @param error - The error that is produced when the tests failed.
+ * @param message - An message on why the test failed.
+ * @param testName - The name of the test.
+ */
 TestRunner.prototype.failTest = function(error,message,testName) {
     this.failed++;
     failed++;
@@ -80,10 +116,16 @@ TestRunner.prototype.failTest = function(error,message,testName) {
     this.failedTests.push(testResult);
     failedTests.push(testResult);
 };
+/**
+ * Succeeds an test.
+ */
 TestRunner.prototype.succeedTest = function() {
     this.passed++;
     passed++;
 };
+/**
+ * Prints the results of all the tests.
+ */
 TestRunner.prototype.printResults = function() {
     var string = "======== " + this.file + " ========";
     console.log(string);
