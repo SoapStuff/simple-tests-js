865f774d8940917eb2b28257d73786a9a3a59aaf
Arjan Seijs <A.M.Seijs@student.tudelft.nl>
2017-10-02 18:21:37 +0200

    Updated so that program wont crash if one of the files isn't an javascript file. Also tests in subfolders now work.

---
+++ SampleTests/submap/SampleTest3.js
@@ -0,0 +1,15 @@
+const Assert = require("assert");
+require("../../util/testRunner").run({
+    test1: function () {
+
+    },
+    test2: function () {
+
+    },
+    test3: {
+        test: function() {
+            Assert.equal(arguments[0]+arguments[1],arguments[2])
+        },
+        parameters: [[1,2,3],[2,3,5],[2,3,6]]
+    }
+},"SampleTest3");
--- simple-test.js
+++ simple-test.js
@@ -1,10 +1,17 @@
 var colors = require("colors");
+var runTests = function(path) {
+    require("fs").readdirSync(path).forEach(function(file) {
+        if(file.indexOf(".js") > - 1) {
+            require(path + '/' + file);
+        } else if(file.indexOf(".") === -1) {
+            runTests(path + '/' + file);
+        }
+    });
+};
+
 module.exports.runTests = function(path) {
     var testRunner = require("./util/testRunner");
-    require("fs").readdirSync(path).forEach(function(file) {
-        //TODO add Tests suffix requirement?
-        require(path + '/' + file);
-    });
+    runTests(path);
     console.log("======== ".yellow + "TEST RESULTS".underline.white +  " ========".yellow);
     console.log(("Passed Tests: " + testRunner.getPassed()).green);
     console.log(("Failed Tests: " + testRunner.getFailed()).red);
